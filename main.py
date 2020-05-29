22320a721f6e7d64cb44a4ea63d2b78076cde99c
Barlingo <dbarrancolinan@gmail.com>
2020-05-29 19:07:25 -0400

    * Added gitignore pyc and venv.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#!./venv/bin/python3
+#!./venv/bin/python
+import sys
+from utils.tubeSearch import *
+
+
+video1 = sourceVideo(results_dict['videos'][1]['id'])
+
+
+for i in range(5):
+    print(i)
